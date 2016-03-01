876f7e42b3c0d2eaefacde4a989c50661a94b8aa
NemoChenTW <nemochentw@gmail.com>
2016-03-01 20:27:25 +0800

    建立 DB.
    dnu restore
    dnvm use 1.0.0-rc1-update1 -p
    dnx ef migrations add InitialDBCreateFinancialAccount
    dnx ef database update

--- src/NTAccounting/Migrations/20160301122457_InitialDBCreateFinancialAccount.Designer.cs
+++ src/NTAccounting/Migrations/20160301122457_InitialDBCreateFinancialAccount.Designer.cs
@@ -8,9 +8,10 @@ using NTAccounting.Models;
 namespace NTAccounting.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160301122457_InitialDBCreateFinancialAccount")]
+    partial class InitialDBCreateFinancialAccount
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
