a36d57890e4e3444751ff1ceda30c480bb09f26c
NemoChenTW <nemochentw@gmail.com>
2016-03-01 19:47:14 +0800

    新增 FinancialAccount member "Name", "Type", "Amount".

--- src/NTAccounting/Models/FinancialAccount.cs
+++ src/NTAccounting/Models/FinancialAccount.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,13 @@ namespace NTAccounting.Models
 {
     public class FinancialAccount
     {
+        [Display(Name = "帳戶名稱")]
+        public string Name { get; set; }
+
+        [Display(Name = "帳戶類型")]
+        public string Type { get; set; }
+
+        [Display(Name = "帳戶餘額")]
+        public string Amount { get; set; }
     }
 }
