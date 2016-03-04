62139e6f2abad3ec60a60c5d12b9d83656948572
NemoChenTW <nemochentw@gmail.com>
2016-03-04 19:06:15 +0800

    新增 Model "FinancialAccountType".

---
+++ src/NTAccounting/Models/FinancialAccountType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NTAccounting.Models
+{
+    public class FinancialAccountType
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "帳戶類型")]
+        public string Type { get; set; }
+    }
+}
