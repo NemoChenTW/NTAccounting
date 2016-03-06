6f8a048cc563203b1cbba2d212af23530ce68960
NemoChenTW <nemochentw@gmail.com>
2016-03-06 15:57:00 +0800

    更新 DB.
    dnx ef migrations add ModifiedFinancialAccountTypeAsForeignKey
    dnx ef database update

--- src/NTAccounting/Migrations/20160306073217_ModifiedFinancialAccountTypeAsForeignKey.Designer.cs
+++ src/NTAccounting/Migrations/20160306073217_ModifiedFinancialAccountTypeAsForeignKey.Designer.cs
@@ -8,9 +8,10 @@ using NTAccounting.Models;
 namespace NTAccounting.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160306073217_ModifiedFinancialAccountTypeAsForeignKey")]
+    partial class ModifiedFinancialAccountTypeAsForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
