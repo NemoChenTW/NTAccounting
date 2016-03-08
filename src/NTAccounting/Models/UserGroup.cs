7e91ab7fbb2a08c8d327f248e1952dffe7de41df
NemoChenTW <nemochentw@gmail.com>
2016-03-08 22:35:48 +0800

    新增 Model "UserGroup".

---
+++ src/NTAccounting/Models/UserGroup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace NTAccounting.Models
+{
+    public class UserGroup
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name = "群組名稱")]
+        public string Name { get; set; }
+
+
+        // 設定與ApplicationUser的關聯
+        public ICollection<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
