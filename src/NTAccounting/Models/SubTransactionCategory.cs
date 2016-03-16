aef5938a1e5ec58f2056414daf1729e5c594d1bf
NemoChenTW <nemochentw@gmail.com>
2016-03-16 17:13:53 +0800

    新增 Model "TransactionMainCategory" & "TransactionSubCategory".

---
+++ src/NTAccounting/Models/SubTransactionCategory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NTAccounting.Models
+{
+    public class SubTransactionCategory
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name = "交易子類別名稱")]
+        public string Name { get; set; }
+
+        //Foreign key for TransactionMainCategory
+        [Display(Name = "所屬交易主類別")]
+        public int MainCategoryID { get; set; }
+
+        [ForeignKey("MainCategoryID")]
+        public virtual MainTransactionCategory MainTransactionCategory { get; set; }
+    }
+}
