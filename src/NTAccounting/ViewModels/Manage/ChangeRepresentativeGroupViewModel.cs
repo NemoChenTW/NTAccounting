0c34d0e8be126da8aa4c006ccf9656538287a53f
NemoChenTW <nemochentw@gmail.com>
2016-05-06 15:30:01 +0800

    新增 ViewModel "ChangeRepresentativeGroup".

---
+++ src/NTAccounting/ViewModels/Manage/ChangeRepresentativeGroupViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NTAccounting.ViewModels.Manage
+{
+    public class ChangeRepresentativeGroupViewModel
+    {
+        [Required]
+        [Display(Name = "使用者群組")]
+        public int RepresentativeGroupID;
+    }
+}
