10faf41d112858410e3fb5b7a07d4594b4e6a504
NemoChenTW <nemochentw@gmail.com>
2016-05-13 16:55:05 +0800

    新增 TransactionsIndexViewModel.

---
+++ src/NTAccounting/ViewModels/Transactions/TransactionsIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NTAccounting.ViewModels.Transactions
+{
+    public class TransactionsIndexViewModel
+    {
+        public List<string> FinacialAccountNameList { get; set; }
+    }
+}
