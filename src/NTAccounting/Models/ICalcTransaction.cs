a2faf25cc3c4936f368a16618192aaf0978cd3cf
NemoChenTW <nemochentw@gmail.com>
2016-05-14 22:54:26 +0800

    新增 interface ICalcTransaction.

---
+++ src/NTAccounting/Models/ICalcTransaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NTAccounting.Models
+{
+    interface ICalcTransaction
+    {
+        void CreateTransaction();
+
+        void UpdateTransaction();
+
+        void DeleteTransaction();
+    }
+}
