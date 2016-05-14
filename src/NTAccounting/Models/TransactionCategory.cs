de4763a8aa1fd2006e86ec05c22d046bd868ecb7
NemoChenTW <nemochentw@gmail.com>
2016-05-14 21:21:36 +0800

    新增 TransactionCategory.

---
+++ src/NTAccounting/Models/TransactionCategory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NTAccounting.Models
+{
+    public abstract class TransactionCategory
+    {
+        public enum TransactionCategoryType
+        {
+            Expense,
+            Income,
+            Transfer
+        }
+
+        [Display(Name = "交易類型")]
+        public TransactionCategoryType TransactionType { get; set; }
+    }
+}
