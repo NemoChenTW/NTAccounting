ebe72e2e6c0afd22f9300e6fec82d4d2ff664859
NemoChenTW <nemochentw@gmail.com>
2016-05-14 22:44:24 +0800

    新增 TransactionTypeName 取得交易類型的中文名稱.

--- src/NTAccounting/Models/TransactionCategory.cs
+++ src/NTAccounting/Models/TransactionCategory.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,5 +19,24 @@ namespace NTAccounting.Models
 
         [Display(Name = "交易類型")]
         public TransactionCategoryType TransactionType { get; set; }
+
+        [NotMapped]
+        public string TransactionTypeName
+        {
+            get
+            { 
+                switch(this.TransactionType)
+                {
+                    case TransactionCategoryType.Expense:
+                        return "支出";
+                    case TransactionCategoryType.Income:
+                        return "收入";
+                    case TransactionCategoryType.Transfer:
+                        return "轉帳";
+                    default:
+                        return "錯誤的交易類型";
+                }
+            }
+        }
     }
 }
